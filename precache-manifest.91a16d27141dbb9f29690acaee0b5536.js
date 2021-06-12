2b131ed527293f2618a2144feecb34394293c7be
Awoyomi Mayowa <mayowaawoyomi@gmail.com>
2021-06-12 10:25:35 +0100

    Updates

---
+++ precache-manifest.91a16d27141dbb9f29690acaee0b5536.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f923351090c88af3392ed2c2877ca848",
+    "url": "/Spotify-clone/index.html"
+  },
+  {
+    "revision": "f165d5d3bd4ba50b6ddc",
+    "url": "/Spotify-clone/static/css/main.0f7b3e73.chunk.css"
+  },
+  {
+    "revision": "8fbeb280bb4d12624f83",
+    "url": "/Spotify-clone/static/js/2.94ef6c67.chunk.js"
+  },
+  {
+    "revision": "355892c9c862d865023da20193b3a40a",
+    "url": "/Spotify-clone/static/js/2.94ef6c67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f165d5d3bd4ba50b6ddc",
+    "url": "/Spotify-clone/static/js/main.b4d32140.chunk.js"
+  },
+  {
+    "revision": "4e41360c900a3c16cc5f",
+    "url": "/Spotify-clone/static/js/runtime-main.f311d068.js"
+  },
+  {
+    "revision": "59fa1409112437a3bd7a0a07963f328e",
+    "url": "/Spotify-clone/static/media/image1.59fa1409.jpeg"
+  }
+]);
